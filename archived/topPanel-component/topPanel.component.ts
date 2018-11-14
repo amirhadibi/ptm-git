a8a318ebcde5b9b8b0cc962b49bb60c449fb4a44
Amir <amir_adibi@hotmail.com>
2018-11-13 19:40:06 -0500

    Remove duplicated directory

--- archived/topPanel-component/topPanel.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'properties',
-  templateUrl: './properties.component.html',
-  styleUrls: ['../css/app.component.css'],
-})
-
-export class PropertiesComponent {}
